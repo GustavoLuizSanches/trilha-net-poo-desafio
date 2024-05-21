0b803a96d7550f0cc54c962c5cb0bf740e9c5a8d
Gustavo Oliveira <gustavo.dukinha@gmail.com>
2024-05-21 16:10:49 -0300

    Desafio concluido!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,27 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string Pessoa)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para {Pessoa}.");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string Pessoa)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação de {Pessoa}.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone (numero: "90101-0101", modelo: "modelo 1", imei: "000000000000000", memoria: 64);
+iphone.Ligar("Luciana");
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia (numero: "90202-0202", modelo: "modelo 2", imei: "111111111111111", memoria: 128);
+nokia.ReceberLigacao("Gustavo");
+nokia.InstalarAplicativo("WhatsApp");
